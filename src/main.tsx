a8122afd38b65224515db8fcd5f9dcc7efc68651
ShahariarNiloy <shahariarniloyblv@gmail.com>
2023-04-16 12:37:07 +0600

    provide and store added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import { positions, transitions, Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+const options = {
+  timeout: 5000,
+  position: positions.BOTTOM_CENTER,
+  transition: transitions.SCALE,
+};
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <AlertProvider template={AlertTemplate} {...options}>
+        <App />
+      </AlertProvider>
+    </Provider>
+  </React.StrictMode>
+);
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,3 @@
 /// <reference types="vite/client" />
 declare module "overlay-navbar";
+declare module "react-alert-template-basic";
